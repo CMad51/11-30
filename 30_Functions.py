44c967071aa5f5775ffc7dfd6c51381bb7a96d22
Cmad51 <mahdi93montazer@gmail.com>
2021-01-24 07:14:37 +0330

    First commit

---
+++ 30_Functions.py
@@ -0,0 +1,14 @@
+#3
+'''
+def add_all_nums (*numbers):
+ _sum=0
+ try:
+  for item in numbers:
+   _sum +=item
+  return ('The sum of numbers=',_sum) 
+ except:
+   return('At least one of your enter numbers is not integer,please check it')
+
+print(add_all_nums (5,'man',8,2))'''
+
+#7
